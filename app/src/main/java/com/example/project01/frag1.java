f3cf54e68497b0a55c13efd999f5fae1fed73576
hyunjeong <gnlgus48@g.skku.edu>
2021-12-28 22:04:59 +0900

    Tab1: ListView

--- app/src/main/java/com/example/project01/frag1.java
+++ app/src/main/java/com/example/project01/frag1.java
@@ -1,6 +1,7 @@
 package com.example.project01;
 
 import android.app.AlertDialog;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,10 +63,52 @@ public class frag1 extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        ListView jList;
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        jList = findViewById(R.id.json_list);
+
+        final ArrayList<String> arrList = new ArrayList<String>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrList);
+
+        jList.setAdapter(arrayAdapter);
+
+        Button btn = (Button)findViewById(R.id.button);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream is = assetManager.open("jsons/test.json");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            StringBuffer buf = new StringBuffer();
+            String line = br.readLine();
+            while(line != null){
+                buf.append(line);
+                line = br.readLine();
+            }
+            String name = null;
+            String number = null;
+            String jsonData = buf.toString();
+            JSONObject jsonObj = new JSONObject(jsonData);
+            JSONArray jsonArr = jsonObj.getJSONArray("book");
+            for (int i = 0; i < jsonArr.length(); i++){
+                JSONObject jObj = jsonArr.getJSONObject(i);
+                name = jObj.optString("name");
+                number = jObj.optString("number");
+                System.out.println(arrList);
+                arrList.add("Name: "+name+"\nNumber: "+number);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
         }
     }
 
