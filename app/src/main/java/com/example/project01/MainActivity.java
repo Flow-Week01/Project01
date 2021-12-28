60722bf7f9dbdc42116ac58f70fe18e6c665f5cd
MINGYEOL KIM <kmg1902@postech.ac.kr>
2021-12-28 21:50:00 +0900

    Tab added

--- app/src/main/java/com/example/project01/MainActivity.java
+++ app/src/main/java/com/example/project01/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new frag1(), "TAB1");
+        vpAdapter.addFragment(new frag2(), "TAB2");
+        vpAdapter.addFragment(new frag3(), "TAB3");
+        viewPager.setAdapter(vpAdapter);
+
     }
 }
