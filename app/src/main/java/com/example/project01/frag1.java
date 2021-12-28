e97368f94ff945ac2948d6d0831cb46fa4d06916
hyunjeong <gnlgus48@g.skku.edu>
2021-12-28 22:21:34 +0900

    Tab1

--- app/src/main/java/com/example/project01/frag1.java
+++ app/src/main/java/com/example/project01/frag1.java
@@ -32,55 +32,49 @@ public class frag1 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
 
     public frag1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment frag1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static frag1 newInstance(String param1, String param2) {
-        frag1 fragment = new frag1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+//    public static frag1 newInstance(String param1, String param2) {
+//        frag1 fragment = new frag1();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+    ListView jList;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+
+        //setContentView(R.layout.fragment_frag1);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        ListView jList;
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-        setContentView(R.layout.activity_main);
-
-        jList = findViewById(R.id.json_list);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_frag1, container, false);
+        jList = v.findViewById(R.id.json_list);
 
         final ArrayList<String> arrList = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrList);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrList);
 
         jList.setAdapter(arrayAdapter);
-
-        Button btn = (Button)findViewById(R.id.button);
-        AssetManager assetManager = getAssets();
+        AssetManager assetManager = getContext().getAssets();
         try {
             InputStream is = assetManager.open("jsons/test.json");
             InputStreamReader isr = new InputStreamReader(is);
@@ -110,12 +104,6 @@ public class frag1 extends Fragment {
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag1, container, false);
+        return v;
     }
 }
