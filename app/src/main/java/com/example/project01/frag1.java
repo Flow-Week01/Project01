6520cecf23fcc38a6afafc82f7ae2eb52814fb23
hyunjeong <gnlgus48@g.skku.edu>
2021-12-29 11:36:53 +0900

    Tab1: AddProfileImage

--- app/src/main/java/com/example/project01/frag1.java
+++ app/src/main/java/com/example/project01/frag1.java
@@ -1,6 +1,5 @@
 package com.example.project01;
 
-import android.app.AlertDialog;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 
@@ -10,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -23,44 +22,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link frag1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class frag1 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
 
     public frag1() {
         // Required empty public constructor
     }
 
-//    public static frag1 newInstance(String param1, String param2) {
-//        frag1 fragment = new frag1();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
     ListView jList;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-
-        //setContentView(R.layout.fragment_frag1);
     }
 
     @Override
@@ -70,10 +41,10 @@ public class frag1 extends Fragment {
         View v = inflater.inflate(R.layout.fragment_frag1, container, false);
         jList = v.findViewById(R.id.json_list);
 
-        final ArrayList<String> arrList = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrList);
+        final ArrayList<ListData> arrList = new ArrayList<>();
+        //final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrList);
 
-        jList.setAdapter(arrayAdapter);
+        //jList.setAdapter(arrayAdapter);
         AssetManager assetManager = getContext().getAssets();
         try {
             InputStream is = assetManager.open("jsons/test.json");
@@ -92,14 +63,17 @@ public class frag1 extends Fragment {
             JSONObject jsonObj = new JSONObject(jsonData);
             JSONArray jsonArr = jsonObj.getJSONArray("book");
             for (int i = 0; i < jsonArr.length(); i++){
+                ListData listData = new ListData();
                 JSONObject jObj = jsonArr.getJSONObject(i);
-                name = jObj.optString("name");
-                number = jObj.optString("number");
-                System.out.println(arrList);
-                arrList.add("Name: "+name+"\nNumber: "+number);
-                arrayAdapter.notifyDataSetChanged();
+                listData.profileImage = R.drawable.ic_launcher_foreground;
+                listData.peopleName = jObj.optString("name");
+                listData.peopleNum = jObj.optString("number");
+                arrList.add(listData);
+                //arrayAdapter.notifyDataSetChanged();
             }
 
+            ListAdapter objAdapter = new CustomListView(arrList);
+            jList.setAdapter(objAdapter);
 
         } catch (IOException | JSONException e) {
             e.printStackTrace();
